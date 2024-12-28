30773dfb66e8ea783014c48b33ab5803e0206962
blxxdclxud <ramazanatzuf10@gmail.com>
2024-12-28 20:22:17 +0300

    The unsupported BLOB type changed to bytea.

--- internal/db/migrations/001_initial.up.sql
+++ internal/db/migrations/001_initial.up.sql
@@ -1,14 +1,14 @@
 CREATE TABLE files (
     Id INT PRIMARY KEY,
     FileName VARCHAR(256),
-    File BLOB,
-    ModifiedDate DATETIME
+    File bytea,
+    ModifiedDate timestamp
 );
 
 CREATE TABLE settings (
     Id INT PRIMARY KEY,
-    ReceiptFile BLOB,
-    Emails BLOB,
+    ReceiptFile bytea,
+    Emails bytea,
     QrPattern VARCHAR(65535),
     SenderEmail VARCHAR(65535)
 );
