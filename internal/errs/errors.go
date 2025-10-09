6db15b56a6928b6e8ba46ce5d3fdea9ad7824de2
blxxdclxud <ramazanatzuf10@gmail.com>
2025-10-09 10:24:36 +0300

    Add common error wrappers

--- internal/errs/errors.go
+++ internal/errs/errors.go
@@ -1,1 +1,11 @@
+// errors.go file contains common pre-defined errors or helper functions
+
 package errs
+
+import "fmt"
+
+// WrapIOError is a helper function for IO operations, that wraps given error,
+// describing it with specified action and filename
+func WrapIOError(action, filename string, err error) error {
+	return Wrap(System, fmt.Sprintf("failed to %s '%s'", action, filename), err)
+}
