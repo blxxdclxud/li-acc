f8666060c865f2c2c80b970705c70718302ad46a
blxxdclxud <ramazanatzuf10@gmail.com>
2025-10-17 01:06:26 +0300

    Add some directories paths as constants

---
+++ internal/model/filepaths.go
@@ -0,0 +1,13 @@
+package model
+
+const (
+	TmpDir = "./tmp"
+
+	PayersXlsDir       = TmpDir + "/payers"
+	EmailXlsDir        = TmpDir + "/emails"
+	ReceiptPatternsDir = TmpDir + "/patterns"
+	SentReceiptsDir    = TmpDir + "/sent"
+	QrCodesDir         = TmpDir + "/qr"
+)
+
+const BlankReceiptPath = "./assets/excel/blank_receipt_pattern.xls"
