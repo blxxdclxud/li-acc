3e2df96335fd65c94b199a53bf7b1e76cf45c83f
blxxdclxud <ramazanatzuf10@gmail.com>
2025-10-17 01:07:02 +0300

    Add Mail model, used by Mail Service

---
+++ internal/model/mail.go
@@ -0,0 +1,31 @@
+package model
+
+import "fmt"
+
+type Mail struct {
+	Subject         string            // subject of the mail
+	Body            string            // body of the message
+	To              []string          // list of emails of the receivers
+	From            string            // sender email
+	AttachmentPaths map[string]string // receiver email -> attachment file path
+}
+
+func (m Mail) GetAttachmentPath(email string) (string, error) {
+	p, ok := m.AttachmentPaths[email]
+	if !ok {
+		return "", fmt.Errorf("no such email in AttachmentPaths")
+	}
+	return p, nil
+}
+
+type SMTP struct {
+	Host     string
+	Port     int
+	Email    string
+	Password string
+}
+
+const (
+	MailDefaultSubject = "Квитанция об оплате ЛИ7"
+	MailDefaultBody    = ""
+)
