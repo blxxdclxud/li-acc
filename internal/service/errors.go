f34212042268baefa48d6952baa24f3bce73fca7
blxxdclxud <ramazanatzuf10@gmail.com>
2025-10-23 07:53:20 +0300

    Add Composite Error type containing multiple custom errors (for ProcessPayersFile)

--- internal/service/errors.go
+++ internal/service/errors.go
@@ -3,7 +3,32 @@ package service
 import (
 	"fmt"
 	"li-acc/internal/errs"
+	"strings"
 )
+
+// CompositeError contains multiple errors together
+type CompositeError struct {
+	Errors []error
+}
+
+func (c *CompositeError) Error() string {
+	var msgs []string
+	for _, e := range c.Errors {
+		msgs = append(msgs, e.Error())
+	}
+	return fmt.Sprintf("multiple errors: [%s]", strings.Join(msgs, "; "))
+}
+
+func (c *CompositeError) Kind() errs.Kind {
+	return errs.User
+}
+
+func (c *CompositeError) Unwrap() []error {
+	if len(c.Errors) == 0 {
+		return nil
+	}
+	return c.Errors
+}
 
 // EmailSendingError error raised when sender.SendMail() returned an error.
 // Implement interface errs.CodedError
@@ -30,4 +55,29 @@ func (e *EmailSendingError) Unwrap() error {
 
 func (e *EmailSendingError) FailedCount() int {
 	return len(e.MapReceiverCause)
+}
+
+// EmailMappingError error raised when there are no emails mapped for some payers
+type EmailMappingError struct {
+	MapPayerReceipt map[string]string // map payer name -> pdf receipt path
+}
+
+func (e *EmailMappingError) Error() string {
+	var msg []string
+	for payer, path := range e.MapPayerReceipt {
+		msg = append(msg, fmt.Sprint(payer, ": ", path))
+	}
+	return fmt.Sprintf("no emails found for some payers. %v", msg)
+}
+
+func (e *EmailMappingError) Kind() errs.Kind {
+	return errs.User
+}
+
+func (e *EmailMappingError) Unwrap() error {
+	return nil
+}
+
+func (e *EmailMappingError) FailedCount() int {
+	return len(e.MapPayerReceipt)
 }
